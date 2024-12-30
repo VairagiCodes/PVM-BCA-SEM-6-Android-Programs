48fbcf5b8a6da6728b90664998aadfeff38c2322
Harsh Ruparelia <rj.harsh3@gmail.com>
2024-12-30 15:11:51 +0530

    Change in Activity LifeCycle
    Method names corrected

--- Android ActivityLifeCycle Programs/app/src/main/java/com/vairagicodes/androidactivitylifecycle/MainActivity.java
+++ Android ActivityLifeCycle Programs/app/src/main/java/com/vairagicodes/androidactivitylifecycle/MainActivity.java
@@ -1,5 +1,4 @@
 package com.vairagicodes.androidactivitylifecycle;
-
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,44 +11,44 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Log.d("Activity LifeCycle","This is one create");
+        Log.d("Activity LifeCycle","This is onCreate");
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Activity LifeCycle","This is one start");
+        Log.d("Activity LifeCycle","This is onStart");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Activity LifeCycle","This is one Resume");
+        Log.d("Activity LifeCycle","This is onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("Activity LifeCycle","This is one Pause");
+        Log.d("Activity LifeCycle","This is onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("Activity LifeCycle","This is one stop");
+        Log.d("Activity LifeCycle","This is onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("Activity LifeCycle","This is one Restart");
+        Log.d("Activity LifeCycle","This is onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("Activity LifeCycle","This is one Destroy");
+        Log.d("Activity LifeCycle","This is onDestroy");
     }
 }
