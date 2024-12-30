db42e6bdd07381329d8b51387d66f016ee2b6c04
Harsh Ruparelia <rj.harsh3@gmail.com>
2024-12-30 14:32:46 +0530

    Implicit Intent

--- ImplicitJava/app/src/main/java/com/vairagicodes/implicitintent/MainActivity.java
+++ ImplicitJava/app/src/main/java/com/vairagicodes/implicitintent/MainActivity.java
@@ -3,7 +3,6 @@ package com.vairagicodes.implicitintent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -30,28 +29,18 @@ public class MainActivity extends AppCompatActivity {
         EditText opeURlEditText = findViewById(R.id.get_url_et);
 
 
-
         Button openUrlButton = findViewById(R.id.open_url_btn);
 
 
+        openUrlButton.setOnClickListener(v -> {
 
-        openUrlButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String url = opeURlEditText.getText().toString().trim();
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Email subject");
-                intent.putExtra(Intent.EXTRA_TEXT, "Email body");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"john.archibald.campbell@example-pet-store.com"});)
-                intent.setData(Uri.parse(url));
-                startActivity(intent);
+            String url = opeURlEditText.getText().toString().trim();
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
 
 
-
-            }
         });
-
 
 
     }
